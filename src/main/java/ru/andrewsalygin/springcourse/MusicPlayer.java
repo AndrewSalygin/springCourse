783c862d66335081d52ee1746cc8c95b065bc197
Andrew Salygin <andrew.salygin@gmail.com>
2023-07-23 19:59:50 +0400

    Added: new variables: name and volume; substituted values in these parameters in ApplicationContexts

--- src/main/java/ru/andrewsalygin/springcourse/MusicPlayer.java
+++ src/main/java/ru/andrewsalygin/springcourse/MusicPlayer.java
@@ -6,6 +6,25 @@ package ru.andrewsalygin.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
     // Inversion of Control
     public MusicPlayer(Music music) {
         this.music = music;
