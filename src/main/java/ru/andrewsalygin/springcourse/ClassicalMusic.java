adbb1370021d43758cb297e76d1ed5836f46e9c2
Andrew Salygin <andrew.salygin@gmail.com>
2023-07-23 18:13:15 +0400

    Change music types

--- src/main/java/ru/andrewsalygin/springcourse/ClassicalMusic.java
+++ src/main/java/ru/andrewsalygin/springcourse/ClassicalMusic.java
@@ -4,8 +4,21 @@ package ru.andrewsalygin.springcourse;
  * @author Andrew Salygin on 23.07.2023
  */
 public class ClassicalMusic implements Music {
+    private String musicName;
+
+    public String getMusicName() {
+        return musicName;
+    }
+
+    public void setMusicName(String musicName) {
+        this.musicName = musicName;
+    }
+
+    public ClassicalMusic(String musicName) {
+        this.musicName = musicName;
+    }
     @Override
     public String getSong() {
-        return "Schenkerian Analysis";
+        return musicName;
     }
 }
--- src/main/java/ru/andrewsalygin/springcourse/RockMusic.java
+++ src/main/java/ru/andrewsalygin/springcourse/RockMusic.java
@@ -4,8 +4,21 @@ package ru.andrewsalygin.springcourse;
  * @author Andrew Salygin on 23.07.2023
  */
 public class RockMusic implements Music {
+    private String musicName;
+
+    public String getMusicName() {
+        return musicName;
+    }
+
+    public void setMusicName(String musicName) {
+        this.musicName = musicName;
+    }
+
+    public RockMusic(String musicName) {
+        this.musicName = musicName;
+    }
     @Override
     public String getSong() {
-        return "Shinedown - Diamond Eyes";
+        return musicName;
     }
 }
