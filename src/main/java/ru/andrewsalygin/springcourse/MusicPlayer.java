916d166957fd1d14899d90a800060db003f49e18
Andrew Salygin <andrew.salygin@gmail.com>
2023-07-23 18:06:05 +0400

    Added MusicPlayer

---
+++ src/main/java/ru/andrewsalygin/springcourse/MusicPlayer.java
@@ -0,0 +1,17 @@
+package ru.andrewsalygin.springcourse;
+
+/**
+ * @author Andrew Salygin on 23.07.2023
+ */
+public class MusicPlayer {
+    private Music music;
+
+    // Inversion of Control
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public void playMusic() {
+        System.out.println("Playing: " + music.getSong());
+    }
+}
