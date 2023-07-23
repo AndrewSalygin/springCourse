c7c55cf5d586005e09d2cf8f5a82368de0121f76
Andrew Salygin <andrew.salygin@gmail.com>
2023-07-23 17:23:21 +0400

    the project is configured

---
+++ src/main/java/ru/andrewsalygin/springcourse/TestSpring.java
@@ -0,0 +1,18 @@
+package ru.andrewsalygin.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Andrew Salygin on 23.07.2023
+ */
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(testBean.getName());
+    }
+}
