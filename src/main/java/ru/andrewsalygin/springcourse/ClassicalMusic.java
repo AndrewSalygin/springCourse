e905bb81050db116e1c6bae3adb6000a8f9ee5c3
Andrew Salygin <andrew.salygin@gmail.com>
2023-07-24 18:54:07 +0400

    Tried @PostConstruct and @PreDestroy

--- src/main/java/ru/andrewsalygin/springcourse/ClassicalMusic.java
+++ src/main/java/ru/andrewsalygin/springcourse/ClassicalMusic.java
@@ -3,15 +3,27 @@ package ru.andrewsalygin.springcourse;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 /**
  * @author Andrew Salygin on 23.07.2023
  */
 
 @Component
-@Scope("prototype")
 public class ClassicalMusic implements Music {
     @Override
     public String getSong() {
         return "Schenkerian Analysis";
+    }
+
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
     }
 }
