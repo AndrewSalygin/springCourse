00e0e1ad7b75a4af352f5662ebcf546b2700c055
Andrew Salygin <andrew.salygin@gmail.com>
2023-07-26 19:36:51 +0400

    Get queries are implemented to display all people and each one individually

---
+++ SpringMVCApp1/src/main/java/ru/andrewsalygin/springcourse/dao/PersonDAO.java
@@ -0,0 +1,34 @@
+package ru.andrewsalygin.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.andrewsalygin.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Andrew Salygin on 26.07.2023
+ */
+
+@Component
+public class PersonDAO {
+    private List<Person> people;
+    private static int PEOPLE_COUNT = 0;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Alex"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Mariya"));
+        people.add(new Person(++PEOPLE_COUNT, "Ekaterina"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
