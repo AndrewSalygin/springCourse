38215c463846b5d671a0808861c1950a9c167c09
Andrew Salygin <andrew.salygin@gmail.com>
2023-07-28 21:47:24 +0400

    Did a field validate

--- SpringMVCApp1/src/main/java/ru/andrewsalygin/springcourse/controllers/PeopleController.java
+++ SpringMVCApp1/src/main/java/ru/andrewsalygin/springcourse/controllers/PeopleController.java
@@ -1,8 +1,10 @@
 package ru.andrewsalygin.springcourse.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.andrewsalygin.springcourse.dao.PersonDAO;
 import ru.andrewsalygin.springcourse.models.Person;
@@ -44,7 +46,11 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "people/new";
+
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -56,7 +62,11 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult, @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
         personDAO.update(id, person);
         return "redirect:/people";
     }
